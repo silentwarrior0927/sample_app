d1b76cbda79f229c27f2a00f9a96f5285a39f45b
Silent Warrior <silentwarrior0927@outlook.com>
2014-04-18 12:54:03 -0400

    Modify config.force_ssl in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.force_ssl = true
+  # config.force_ssl = true
 end
