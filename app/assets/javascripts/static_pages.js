c4468c9a6aff82f0d9ed35ec277940f2683ffe02
Silent Warrior <silentwarrior0927@outlook.com>
2014-06-10 17:33:32 -0400

    Add minor changes

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
 	$("#request_trial_button").click(function (event) {
-		$("#request_trial_form").css("display", "block");
 		$(this).hide();
 		event.preventDefault(); // Prevent link from following its href
 		var h = $("#request_trial_form").height();
 		var topMargin = (88 - h) / 2;
 		$("#request_trial_form_wrapper").css("top", topMargin);
+		setTimeout(function(){$('#free_trial_field').focus(); }, 0);
 	});
 
 });
