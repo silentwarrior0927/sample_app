c852aa85f9e7958bb6cc1f64619396aa61455b10
Silent Warrior <silentwarrior0927@outlook.com>
2014-06-11 17:08:34 -0400

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326014835) do
+ActiveRecord::Schema.define(version: 20140610162828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "free_trials", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sales", force: true do |t|
     t.date     "bankruptcy_filing_date",                               default: [], array: true
